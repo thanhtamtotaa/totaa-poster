9e44767d90266bc77bddd993ad5984858a7c96f0
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-17 17:48:03 +0700

    Update Poster_Role_Permission_Seeder.php

--- database/seeders/Poster_Role_Permission_Seeder.php
+++ database/seeders/Poster_Role_Permission_Seeder.php
@@ -15,6 +15,30 @@ class Poster_Role_Permission_Seeder extends Seeder
      */
     public function run()
     {
+        if (Role::where("name", "super-admin")->count() == 0) {
+            $super_admin =  Role::create(['name' => 'super-admin', "description" => "Super Admin", "group" => "Admin", "order" => 1, "lock" => true,]);
+        } else {
+            $super_admin= Role::where("name", "super-admin")->first();
+        }
+
+        if (Role::where("name", "admin")->count() == 0) {
+            $admin = Role::create(['name' => 'admin', "description" => "Admin", "group" => "Admin", "order" => 2, "lock" => true,]);
+        } else {
+            $admin = Role::where("name", "admin")->first();
+        }
+
+        if (Role::where("name", "admin-poster")->count() == 0) {
+            $admin_poster = Role::create(['name' => 'admin-poster', "description" => "Admin Quản lý Poster", "group" => "Admin", "order" => 2, "lock" => true,]);
+        } else {
+            $admin_poster = Role::where("name", "admin-poster")->first();
+        }
+
+        if (Role::where("name", "khaosat-poster")->count() == 0) {
+            $khaosat_poster = Role::create(['name' => 'khaosat-poster', "description" => "Khảo sát Poster", "group" => "Poster", "order" => 2, "lock" => true,]);
+        } else {
+            $khaosat_poster = Role::where("name", "khaosat-poster")->first();
+        }
+
         if (Permission::where("name", "view-poster")->count() == 0) {
             $permission[] = Permission::create(['name' => 'view-poster', "description" => "Xem Poster", "group" => "Poster", "order" => 1, "lock" => true,]);
         } else {
@@ -37,24 +61,6 @@ class Poster_Role_Permission_Seeder extends Seeder
             $permission[] = Permission::create(['name' => 'delete-poster', "description" => "Xóa Poster", "group" => "Poster", "order" => 4, "lock" => true,]);
         } else {
             $permission[] = Permission::where("name", "delete-poster")->first();
-        }
-
-        if (Role::where("name", "super-admin")->count() == 0) {
-            $super_admin =  Role::create(['name' => 'super-admin', "description" => "Super Admin", "group" => "Admin", "order" => 1, "lock" => true,]);
-        } else {
-            $super_admin= Role::where("name", "super-admin")->first();
-        }
-
-        if (Role::where("name", "admin")->count() == 0) {
-            $admin = Role::create(['name' => 'admin', "description" => "Admin", "group" => "Admin", "order" => 2, "lock" => true,]);
-        } else {
-            $admin = Role::where("name", "admin")->first();
-        }
-
-        if (Role::where("name", "admin-poster")->count() == 0) {
-            $admin_poster = Role::create(['name' => 'admin-poster', "description" => "Admin Quản lý Poster", "group" => "Admin", "order" => 2, "lock" => true,]);
-        } else {
-            $admin_poster = Role::where("name", "admin-poster")->first();
         }
 
         $super_admin->givePermissionTo($permission);
