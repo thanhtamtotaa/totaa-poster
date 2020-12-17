0e85a063b8f6c77b973853e0f300018fc5e1fd02
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-17 13:47:28 +0700

    Create PosterCaNhanDataTable.php

---
+++ src/DataTables/PosterCaNhanDataTable.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Totaa\TotaaPoster\DataTables;
+
+use Totaa\TotaaPoster\Models\Poster\Poster_List;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class PosterCaNhanDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables()
+            ->eloquent($query)
+            ->addColumn('action', 'poster_list.action');
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \Totaa\TotaaPoster\Models\Poster\Poster_List $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(Poster_List $model)
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->setTableId('poster_list-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->dom('Bfrtip')
+                    ->orderBy(1);
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            Column::computed('action')
+                  ->exportable(false)
+                  ->printable(false)
+                  ->width(60)
+                  ->addClass('text-center'),
+            Column::make('id'),
+            Column::make('add your columns'),
+            Column::make('created_at'),
+            Column::make('updated_at'),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'Poster_List_' . date('YmdHis');
+    }
+}
