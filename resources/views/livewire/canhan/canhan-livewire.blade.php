d6a004e549059d77012810b717ad919e6f5b30ba
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-17 17:48:11 +0700

    Update canhan-livewire.blade.php

--- resources/views/livewire/canhan/canhan-livewire.blade.php
+++ resources/views/livewire/canhan/canhan-livewire.blade.php
@@ -1,3 +1,17 @@
 <div>
-ádasd
+    <!-- Filters and Add Buttons -->
+    @include('totaa-poster::livewire.canhan.support.filters')
+
+    <!-- Incluce các modal -->
+    @include('totaa-poster::livewire.canhan.modal.add_edit_modal')
+
+    <!-- Scripts -->
+    @push('livewires')
+        @include('totaa-poster::livewire.canhan.support.script')
+    @endpush
+
+    <!-- Style -->
+    @push('styles')
+        @include('totaa-poster::livewire.canhan.support.style')
+    @endpush
 </div>
