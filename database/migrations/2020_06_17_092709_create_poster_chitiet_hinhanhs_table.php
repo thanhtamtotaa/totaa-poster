c972db1e66f96ac4f342bc8035e1d9ca678fd7d4
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-17 11:51:48 +0700

    Create 2020_06_17_092709_create_poster_chitiet_hinhanhs_table.php

---
+++ database/migrations/2020_06_17_092709_create_poster_chitiet_hinhanhs_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Totaa\TotaaBfo\Models\BfoInfo;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Totaa\TotaaPoster\Models\Poster\Poster_ChiTiet;
+
+class CreatePosterChitietHinhanhsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('poster_chitiet_hinhanhs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('poster_chitiet_id')->unsigned()->nullable()->default(null);
+            $table->longText('google_drive_id')->nullable()->default(null);
+            $table->boolean('active')->nullable()->default(null);
+            $table->string('belongto_mnv', 10)->nullable()->default(null);
+            $table->string('created_by_mnv', 10)->nullable()->default(null);
+            $table->unsignedBigInteger('created_by')->nullable()->default(null);
+            $table->unsignedBigInteger('updated_by')->nullable()->default(null);
+            $table->unsignedBigInteger('deleted_by')->nullable()->default(null);
+            $table->timestamps();
+            $table->softDeletes();
+
+            if (class_exists(Poster_ChiTiet::class)) {
+                $table->foreign('poster_chitiet_id')->references('id')->on(with(new Poster_ChiTiet)->getTable())->onDelete('SET NULL')->onUpdate('cascade');
+            }
+
+            if (class_exists(BfoInfo::class)) {
+                $table->foreign('belongto_mnv')->references('mnv')->on(with(new BfoInfo)->getTable())->onDelete('SET NULL')->onUpdate('cascade');
+                $table->foreign('created_by_mnv')->references('mnv')->on(with(new BfoInfo)->getTable())->onDelete('SET NULL')->onUpdate('cascade');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('poster_chitiet_hinhanhs');
+    }
+}
