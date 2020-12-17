58ce4356aefc7c4fd4863718d7e9d5a49cf2fdc6
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-17 11:18:59 +0700

    Update DiaDiem_List.php

--- src/Models/DiaDiem/DiaDiem_List.php
+++ src/Models/DiaDiem/DiaDiem_List.php
@@ -2,6 +2,7 @@
 
 namespace Totaa\TotaaPoster\Models\DiaDiem;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Wildside\Userstamps\Userstamps;
@@ -21,5 +22,25 @@ class DiaDiem_List extends Model
 
     // Disable Laravel's mass assignment protection
     protected $guarded = ['id'];
+
+    /**
+     * Mỗi địa điểm thuộc một phân loại
+     *
+     * @return void
+     */
+    public function loaidiadiem()
+    {
+        return $this->belongsTo('Totaa\TotaaPoster\Models\DiaDiem\DiaDiem_PhanLoai', 'loaidiadiem_id', 'id');
+    }
+
+    /**
+     * Mỗi địa điểm nằm ở một xã nào đó
+     *
+     * @return void
+     */
+    public function xa()
+    {
+        return $this->belongsTo('Totaa\TotaaDonvi\Models\TotaaXa', 'xa_id', 'id');
+    }
 
 }
