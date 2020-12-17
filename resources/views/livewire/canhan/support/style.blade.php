a68c96db177c2159557a45def7e6618fb210ca38
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-17 17:48:21 +0700

    Create style.blade.php

---
+++ resources/views/livewire/canhan/support/style.blade.php
@@ -0,0 +1,8 @@
+<style>
+    [cpc1hn-datatables] > thead > tr > th:nth-child(23),
+    [cpc1hn-datatables] > tbody > tr > td:nth-child(23) {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        max-width: 450px;
+    }
+</style>
