0330016b86cdc8e1a3835ee3628c4cf66036496f
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-17 11:19:03 +0700

    Create Poster_HinhThuc.php

---
+++ src/Models/Poster/Poster_HinhThuc.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Totaa\TotaaPoster\Models\Poster;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Wildside\Userstamps\Userstamps;
+
+class Poster_HinhThuc extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    use Userstamps;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'poster_hinhthucs';
+
+    // Disable Laravel's mass assignment protection
+    protected $guarded = ['id'];
+
+}
