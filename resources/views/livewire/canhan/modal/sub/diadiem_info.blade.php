6061c41ca22f2fdbe9f95b14c62d461c2e777405
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-25 17:20:52 +0700

    Create diadiem_info.blade.php

---
+++ resources/views/livewire/canhan/modal/sub/diadiem_info.blade.php
@@ -0,0 +1,76 @@
+<div class="col-12 col-xl-6 mb-3 h-max-content">
+    <div class="row">
+
+        <div class="col-md-12">
+            <div class="form-group">
+                <h5 class="text-info">Thông tin địa điểm:</h5>
+            </div>
+        </div>
+
+        @if (!!$diadiem)
+
+        @php
+            $xa = $diadiem->xa;
+            $huyen = $xa->huyen;
+            $tinh = $huyen->tinh;
+        @endphp
+
+            <div class="col-12 col-md-6">
+                <div class="form-group">
+                    <label class="col-form-label">Phân loại địa điểm:</label>
+                    <div>
+                        <span type="text" class="form-control px-2 h-auto">{{ $diadiem->loaidiadiem->name }}</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6">
+                <div class="form-group">
+                    <label class="col-form-label">Tên địa điểm:</label>
+                    <div>
+                        <span type="text" class="form-control px-2 h-auto">{{ $diadiem->tendiadiem }}</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6">
+                <div class="form-group">
+                    <label class="col-form-label">Chủ địa điểm:</label>
+                    <div>
+                        <span type="text" class="form-control px-2 h-auto">{{ $diadiem->chudiadiem }}</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6">
+                <div class="form-group">
+                    <label class="col-form-label">Số điện thoại:</label>
+                    <div>
+                        <span type="text" class="form-control px-2 h-auto">{{ $diadiem->phone }}</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="form-group">
+                    <label class="col-form-label">Địa chỉ:</label>
+                    <div>
+                        <span type="text" class="form-control px-2 h-auto">{{ $diadiem->diachi." - ".$xa->level." ".$xa->name." - ".$huyen->level." ".$huyen->name." - ".$tinh->level." ".$tinh->name }}</span>
+                    </div>
+                </div>
+            </div>
+
+            @if (!!$diadiem->thongtinkhac)
+                <div class="col-12">
+                    <div class="form-group">
+                        <label class="col-form-label">Thông tin khác:</label>
+                        <div>
+                            <pre class="form-control px-2 h-auto">{{ $diadiem->thongtinkhac }}</pre>
+                        </div>
+                    </div>
+                </div>
+            @endif
+
+        @endif
+    </div>
+</div>
